9054785bc0908b40b0a2759ebf5d7149efbcf1f0
er-priyanka <shahanepriyankas01@gmail.com>
2024-05-21 16:19:45 +0530

    connected to database

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -1,1 +1,10 @@
 // connection with mongodb altas
+const mongoose = require('mongoose');
+require("dotenv").config();
+
+const MONGO_URL = process.env.Mongo_URL;
+
+const connection = mongoose.connect(MONGO_URL);
+
+module.exports = { connection };
+
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,31 @@
 // main file
 const express = require('express');
-const dotenv = require('dotenv');
+require("dotenv").config();
+const { connection } = require("./Config/db");
+const cors = require('cors');
 
-dotenv.config();
+
+const PORT = process.env.PORT;
 
 const app = express();
 
-app.listen(8080, ()=>{
-    console.log(`server running on port 8080`);
-})
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(cors());
+
+
+
+
+app.get('/', (req, res) => res.send('Hello'));
+
+app.listen(PORT, async () => {
+    try{
+        await connection;
+        console.log("Connected to db");
+    }catch(err){
+        console.log(err);
+    }
+    console.log(`server started on http://localhost:${PORT}`)
+
+});
