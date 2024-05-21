690f7e11f95253d03066a59acc2fc4dc6186b3a7
er-priyanka <shahanepriyankas01@gmail.com>
2024-05-21 21:42:54 +0530

    Implemented backend task route endpoints

--- backend/Models/task.model.js
+++ backend/Models/task.model.js
@@ -1,1 +1,35 @@
 // task schema and model
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+    title: {
+        type:String,
+        required:true
+    },
+    description: {
+        type:String,
+        required:true
+    },
+    dueDate: {
+        type:Date,
+        required:true
+    },
+    status:{
+        type:String,
+        enum:["Pending", "Completed"],
+        default:"Pending",
+        required: true
+    },
+    image:{
+        type:String
+    },
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    }
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = { Task };
